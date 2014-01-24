3f8906e58bbf9d907f92cafd67c27224dae1c2d6
Matt Corallo <coingenbot@bluematt.me>
2014-01-24 18:57:55 +0100

    Change names

--- share/qt/make_windows_icon.sh
+++ share/qt/make_windows_icon.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # create multiresolution windows icon
-ICON_SRC=../../src/qt/res/icons/bitcoin.png
-ICON_DST=../../src/qt/res/icons/bitcoin.ico
-convert ${ICON_SRC} -resize 16x16 bitcoin-16.png
-convert ${ICON_SRC} -resize 32x32 bitcoin-32.png
-convert ${ICON_SRC} -resize 48x48 bitcoin-48.png
-convert bitcoin-16.png bitcoin-32.png bitcoin-48.png ${ICON_DST}
+ICON_SRC=../../src/qt/res/icons/dongcoin.png
+ICON_DST=../../src/qt/res/icons/dongcoin.ico
+convert ${ICON_SRC} -resize 16x16 dongcoin-16.png
+convert ${ICON_SRC} -resize 32x32 dongcoin-32.png
+convert ${ICON_SRC} -resize 48x48 dongcoin-48.png
+convert dongcoin-16.png dongcoin-32.png dongcoin-48.png ${ICON_DST}
 
--- src/qt/bitcoinunits.h
+++ src/qt/bitcoinunits.h
@@ -19,9 +19,9 @@ public:
      */
     enum Unit
     {
-        BTC,
-        mBTC,
-        uBTC
+        DNG,
+        mDNG,
+        uDNG
     };
 
     //! @name Static API
